e2122bd652fbf29a1b047d8496b081e358ef98d0
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-06-18 02:20:39 +0500

    remove path

--- app/workWithDB/WorkWithDB.py
+++ app/workWithDB/WorkWithDB.py
@@ -1,7 +1,7 @@
 import json
 
 class WorkWithDb:
-    path = "C:/Users/Proger/Documents/AI-expert_call/app/workWithDB/db.json" # путь до db.json
+    path = "YOUR PATH" # путь до db.json
     @staticmethod
     def show_characteristics(drone_name: str) -> dict:
         with open(WorkWithDb.path, 'r') as file:
